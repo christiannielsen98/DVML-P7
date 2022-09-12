3b12384e50915822360aba0d08b9f269e3e4cd5a
Divixity <56671866+Divixity@users.noreply.github.com>
2022-09-12 15:42:03 +0200

    Updates to get file path

---
+++ Code/get_data_path.py
@@ -0,0 +1,9 @@
+import main
+import os
+
+
+def get_path(file):
+    with open(os.path.join(main.root, "Config", "data_path.txt")) as f:
+        path = f.readline()
+
+    return os.path.join(path, file)
