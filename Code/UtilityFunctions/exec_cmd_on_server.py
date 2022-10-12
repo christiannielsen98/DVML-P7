1396e6946da4b58eb690ea15816b951e26c9c13f
christiannielsen98 <56675603+christiannielsen98@users.noreply.github.com>
2022-10-12 23:50:21 +0200

    small optim

--- Code/UtilityFunctions/exec_cmd_on_server.py
+++ Code/UtilityFunctions/exec_cmd_on_server.py
@@ -1,11 +1,11 @@
 from UtilityFunctions.run_query import run_query
 import paramiko
 
+host = '10.92.0.48'
+user = 'ubuntu'
+keyfilename = '/Users/christiannielsen/Library/CloudStorage/OneDrive-AalborgUniversitet/DVML-P7/P7.pem'
 
 def exec_cmd_on_server(cmd_to_execute: str):
-    host = '10.92.0.48'
-    user = 'ubuntu'
-    keyfilename = '/Users/christiannielsen/Library/CloudStorage/OneDrive-AalborgUniversitet/DVML-P7/P7.pem'
     # cmd_to_execute = f'docker exec -i vos isql 1111 exec="{sparql_query}"'
     cmd_to_execute = f"""{cmd_to_execute}"""
 
@@ -22,9 +22,6 @@ def exec_cmd_on_server(cmd_to_execute: str):
     return ssh_stdout
 
 def exec_sparql_on_server(sparql_query: str):
-    host = '10.92.0.48'
-    user = 'ubuntu'
-    keyfilename = '/Users/christiannielsen/Library/CloudStorage/OneDrive-AalborgUniversitet/DVML-P7/P7.pem'
     # cmd_to_execute = f'docker exec -i vos isql 1111 exec="{sparql_query}"'
     cmd_to_execute = f"""cd /home/ubuntu/DVML-P7/Code; python3.10 -c 'from UtilityFunctions.run_query import run_query; run_query(query="{sparql_query}", as_dataframe=False, do_print=True)'"""
 
