767a733eace8b2e79cf94bbb8b5ff6137e8eb9f8
Mads Corfixen <mcorfi16@student.aau.dk>
2022-11-07 18:39:31 +0100

    prep for wikidata graphs

--- Code/Development/wikidata_locations_graph.py
+++ Code/Development/wikidata_locations_graph.py
@@ -1,40 +1,63 @@
 import pandas as pd
-from rdflib import Namespace, Graph, URIRef, Literal
-from rdflib.namespace import RDFS, XSD
+from rdflib import Graph, Literal, Namespace, URIRef
+from rdflib.namespace import XSD
 
+import Code.UtilityFunctions.get_data_path as adsga
 from Code.UtilityFunctions.get_data_path import get_path
 
-WIKIDATA_ENTITY_NS = Namespace("http://www.wikidata.org/entity/")
-WIKIDATA_PREDICATE_NS = Namespace("https://www.wikidata.org/wiki/Property:")
+wd_entity_ns = Namespace("http://www.wikidata.org/entity/")
+wd_predicate_ns = Namespace("https://www.wikidata.org/wiki/Property:")
 
 
-def wikidata_predicate_lookup(column_name: str) -> str:
+def wikidata_predicate_lookup(column_name: str) -> tuple[str, XSD.anyURI]:
     match column_name:
         case "Population":
-            pass
+            return wd_predicate_ns + "P1082", XSD.integer
         case "City":
-            return WIKIDATA_PREDICATE_NS + "P1448", XSD.string
+            pass
+        case "County":
+            pass
         case "State":
             pass
         case "Province":
             pass
 
 
-def wikidata_locations(csv_file: str):
-    df = pd.read_csv(get_path(f"{csv_file}.csv"))
+def wikidata_locations(csv_file: str) -> None:
+    country_Qid = [("usa" "Q30"), ("canada", "Q16")]
+    granularity_Qid = [("States", "Q7275"), ("Counties", "Q28575"), ("Cities", "Q515")]
     G = Graph()
-    for row_tuple in df.itertuples:
+    for country, Qid in country_Qid:
+
+        G.add(triple=(
+            URIRef(wd_entity_ns + Qid),  # e.g., USA
+            URIRef(wd_predicate_ns + "P31"),  # Instance of
+            URIRef(wd_entity_ns + "Q6256")  # Country
+        ))
+
+        for granularity, Qid in granularity_Qid:
+            df = pd.read_csv(f"{country}{granularity}")
+            for tuple in df.itertuples():
+                pass
+            pass
+        pass
+
+
+    df = pd.read_csv(adsga.get_path(f"{csv_file}"))
+    print(df)
+    G = Graph()
+    for tuple in df.itertuples():
+        predicate, object_type = wikidata_predicate_lookup("Population")
         G.add(
             triple=(
-                URIRef(row_tuple.CityCode),
-                URIRef(wikidata_predicate_lookup("City")[0]),
-                Literal(row_tuple.City, datatype=wikidata_predicate_lookup("City")[1])
+                URIRef(tuple.CityCode),
+                URIRef(predicate),
+                Literal(tuple.Population, datatype=object_type)
             )
         )
 
-    G.serialize("test.nt")
+    G.serialize("test.nt", format="nt")
 
 
 if __name__ == "__main__":
-    wikidata_locations("usCity.csv")
-
+    wikidata_locations("usCities.csv")
