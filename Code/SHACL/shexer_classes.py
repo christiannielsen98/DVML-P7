27c558a12108ac17989ba988e690eb72dce61593
christiannielsen98 <cfpn19@student.aau.dk>
2022-12-14 08:45:23 +0100

    shexer update

--- Code/SHACL/shexer_classes.py
+++ Code/SHACL/shexer_classes.py
@@ -5,16 +5,11 @@ import time
 start = time.time()
 
 target_classes = [
-    "https://schema.org/LocalBusiness",
-    "https://schema.org/Person",
-    "https://schema.org/UserReview",
-    "https://purl.archive.org/purl/yelp/ontology#Tip"
+    ["https://schema.org/LocalBusiness"],
+    ["https://schema.org/Person"],
+    ["https://schema.org/UserReview"],
+    ["https://purl.archive.org/purl/yelp/ontology#Tip"]
 ]
-
-# target_classes = [
-#     "http://example.org/Person",
-#     "http://example.org/Gender"
-# ]
 
 namespaces_dict = {"https://schema.org/": "schema",
                    "http://example.org/": "example",
@@ -30,7 +25,8 @@ namespaces_dict = {"https://schema.org/": "schema",
 input_nt_file = "/home/ubuntu/vol1/virtuoso/import/yelp_business.nt"
 # input_nt_file = "/home/ubuntu/DVML-P7/Code/Development/target_graph.nt"
 
-idx = 1
+idx_threshold = 0
+idx_input = 3
 input_files = ["/home/ubuntu/vol1/virtuoso/import/yelp_business.nt",
                "/home/ubuntu/vol1/virtuoso/import/yelp_user.nt",
                "/home/ubuntu/vol1/virtuoso/import/yelp_review.nt",
@@ -38,12 +34,12 @@ input_files = ["/home/ubuntu/vol1/virtuoso/import/yelp_business.nt",
 classes = ["business", "user", "review", "tip"]
 thresholds = [0.25, 0.2, 0.1, 0.05, 0.01]
 
-shaper = Shaper(target_classes=target_classes, graph_file_input=input_files[idx], namespaces_dict=namespaces_dict, input_format=NT,
+shaper = Shaper(target_classes=target_classes[idx_input], graph_file_input=input_files[idx_input], namespaces_dict=namespaces_dict, input_format=NT,
                 instantiation_property="http://www.w3.org/2000/01/rdf-schema#Class")  # The predicate which is used to assign the target_classes to the subjects
 
-output_file_shex = f"/home/ubuntu/DVML-P7/Code/SHACL/shaper_{classes[idx]}_{thresholds[idx]}.shex"
+output_file_shex = f"/home/ubuntu/DVML-P7/Code/SHACL/shaper_{classes[idx_input]}_{thresholds[idx_threshold]}.shex"
 
-shaper.shex_graph(output_file=output_file_shex, acceptance_threshold=thresholds[idx])
+shaper.shex_graph(output_file=output_file_shex, acceptance_threshold=thresholds[idx_threshold])
 
 # output_file_ttl = "/home/ubuntu/DVML-P7/Code/Development/shaper_yelp_business.ttl"
 # shaper.shex_graph(output_file=output_file_ttl, acceptance_threshold=0.9, output_format=SHACL_TURTLE)
